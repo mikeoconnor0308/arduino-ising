e3a306462f895ad7d29c4fb6650a37bcf681dfcc
Mike O'Connor <mikeoconnor0308@gmail.com>
2018-04-21 23:01:22 +0100

    functioning Ising model

--- ising.ino
+++ ising.ino
@@ -1,66 +1,104 @@
 #include <Adafruit_LEDBackpack.h>
-
 #include <Adafruit_GFX.h>
 #include <Adafruit_SPITFT_Macros.h>
 #include <Adafruit_SPITFT.h>
 #include <gfxfont.h>
-
-/*************************************************** 
-  This is a library for our I2C LED Backpacks
-
-  Designed specifically to work with the Adafruit LED Matrix backpacks 
-  ----> http://www.adafruit.com/products/872
-  ----> http://www.adafruit.com/products/871
-  ----> http://www.adafruit.com/products/870
-
-  These displays use I2C to communicate, 2 pins are required to 
-  interface. There are multiple selectable I2C addresses. For backpacks
-  with 2 Address Select pins: 0x70, 0x71, 0x72 or 0x73. For backpacks
-  with 3 Address Select pins: 0x70 thru 0x77
-
-  Adafruit invests time and resources providing this open source code, 
-  please support Adafruit and open-source hardware by purchasing 
-  products from Adafruit!
-
-  Written by Limor Fried/Ladyada for Adafruit Industries.  
-  BSD license, all text above must be included in any redistribution
- ****************************************************/
-
 #include <Wire.h>
-#include <Adafruit_GFX.h>
-#include "Adafruit_LEDBackpack.h"
 
-Adafruit_BicolorMatrix matrix = Adafruit_BicolorMatrix();
+Adafruit_BicolorMatrix ledMatrix = Adafruit_BicolorMatrix();
 bool mainLoop = false;
+
+const int xLength = 8;
+const int yLength = 8;
+//current configuration of Ising model. 
+int config[xLength][yLength];
+float temperature = 2.3;
+
+void GenerateInitialConfiguration()
+{
+    for(int i=0; i < xLength; i++)
+    {
+        for(int j =0; j < xLength; j++)
+        {
+            //Generates either 0 or -1;
+            config[i][j] = 2 * random(0, 2) - 1;
+            
+        }
+    }
+}
 
 void setup()
 {
     Serial.begin(9600);
     Serial.println("Ising Model Test");
 
-    matrix.begin(0x70); // pass in the address
+    randomSeed(analogRead(0));
+    ledMatrix.begin(0x70); // pass in the address
+    GenerateInitialConfiguration();
+}
+
+void UpdateLEDMatrix()
+{
+    ledMatrix.clear();
+    for(int x =0; x < xLength; x++)
+    {
+        for(int y =0; y < yLength; y++)
+        {
+            if(config[x][y] == 1) 
+                ledMatrix.drawPixel(x,y, LED_GREEN);
+            else if(config[x][y] == -1)
+                ledMatrix.drawPixel(x,y, LED_RED);
+        }
+        
+    }
+    ledMatrix.writeDisplay();
+}
+
+void MonteCarloMove(float beta)
+{
+    for(int i=0; i < xLength; i++)
+    {
+        for(int j=0; j < yLength; j++)
+        {
+            int x = random(0, xLength); 
+            int y = random(0, yLength); 
+            int state = config[x][y];
+            int nb = (int) config[(x+1)%xLength][y] + (int) config[x][(y+1)%yLength] + (int) config[(x-1)%xLength][y] + (int) config[x][(y-1)%yLength];
+            int cost = 2 * state * nb;
+            if(cost < 0)
+                state *= -1;
+            else 
+            {
+                float prob = (float) random(0, 100) / (float) 100; 
+                if(prob < exp(-cost * beta)) 
+                    state *= -1; 
+            }
+            config[x][y] = state;
+        }
+    }
 }
 
 void loop()
 {
-
-    matrix.clear(); // clear display
-    matrix.drawPixel(0, 0, LED_GREEN);
-    matrix.writeDisplay(); // write the changes we just made to the display
-    delay(500);
-
     if (!mainLoop)
     {
-        matrix.setTextWrap(false); // we dont want text to wrap so it scrolls nicely
-        matrix.setTextSize(1);
-        matrix.setTextColor(LED_GREEN);
+        ledMatrix.setTextWrap(false);
+        ledMatrix.setTextSize(1);
+        ledMatrix.setTextColor(LED_GREEN);
         for (int8_t x = 7; x >= -36; x--)
         {
-            matrix.clear();
-            matrix.setCursor(x, 0);
-            matrix.print("Ising");
-            matrix.writeDisplay();
-            delay(100);
+            ledMatrix.clear();
+            ledMatrix.setCursor(x, 0);
+            ledMatrix.print("Ising");
+            ledMatrix.writeDisplay();
+            delay(30);
         }
+        mainLoop = true;
+
     }
+
+    MonteCarloMove(1.0/temperature);
+    UpdateLEDMatrix();
+    delay(50);
+
 }
